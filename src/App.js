02f0376553b7a47bafe78551c712ac4fc93e3a07
JosiaAlmeida <>
2021-05-23 15:55:20 +0100

    Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Jsx from './component/Jsx'
 
 function App() {
+  const temperatura =20
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, Word</h1>
+      {temperatura >20 ? <Jsx /> : "Sem component"}
     </div>
   );
 }
---
+++ src/component/Jsx.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function jsx(){
+    return(
+        <div>
+            <h1>Eu sou um component</h1>
+        </div>
+    );
+}
